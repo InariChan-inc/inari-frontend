95857dc253b173ce095cceb60fd5e2c6e6fcdef2
Vitalii Shulhan <vitalii.shulhan@gmail.com>
2021-06-28 01:27:23 +0200

    Initial setup

---
+++ .storybook/main.js
@@ -0,0 +1,11 @@
+module.exports = {
+  "stories": [
+    "../atoms/**/*.stories.@(js|jsx|ts|tsx)",
+    "../molecules/**/*.stories.@(js|jsx|ts|tsx)",
+    "../stories/Introduction.stories.mdx",
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials"
+  ]
+}
