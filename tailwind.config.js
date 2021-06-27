e9479de8100e53545ec3cdcaeb29d033e46d1c87
Vitalii Shulhan <vitalii.shulhan@gmail.com>
2021-06-28 01:36:48 +0200

    Modified tailwind config: added all folders

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,13 @@
 module.exports = {
   mode: 'jit',
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  purge: [
+    './atoms/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}', 
+    './containers/**/*.{js,ts,jsx,tsx}',
+    './layouts/**/*.{js,ts,jsx,tsx}',
+    './molecules/**/*.{js,ts,jsx,tsx}', 
+    './pages/**/*.{js,ts,jsx,tsx}', 
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
