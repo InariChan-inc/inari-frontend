1b22800c291e89f86c00ddd473a5e878e10bc7ae
Vitalii Shulhan <vitalii.shulhan@gmail.com>
2021-07-24 01:18:24 +0200

    Added Formik to SearchInput; Implemented debounced form submitting after changing with reactive regex validation

--- molecules/SearchInput/v01/index.tsx
+++ molecules/SearchInput/v01/index.tsx
@@ -1,5 +1,9 @@
-import { FunctionComponent } from 'react';
+import { FunctionComponent, useCallback } from 'react';
 import { Search } from '../../../atoms/icons';
+import {
+  Formik, FormikErrors
+} from 'formik';
+import _ from 'lodash';
 
 interface SearchInputProps {
     placeholder: string,
@@ -8,16 +12,41 @@ interface SearchInputProps {
 }
 
 const SearchInput: FunctionComponent<SearchInputProps> = props => (
-    <div className="bg-yellow-1 py-2 px-11 rounded-tl-full rounded-br-full w-1/2 h-full flex items-center">
-        <label className="flex flex-1 items-center">
+  <Formik
+    initialValues={{ search: '' }}
+    onSubmit={({ search }) => {
+      console.log('SEARCH:', search)
+    }}
+  >
+    {({
+      values: { search },
+      handleChange,
+      submitForm      
+    }) => {
+      const debouncedSubmitForm = useCallback(_.debounce(submitForm, 1000), []);
+
+      return(
+        <div className="bg-yellow-1 py-2 px-11 rounded-tl-full rounded-br-full w-1/2 h-full flex items-center">
+          <label className="flex flex-1 items-center">
             <Search className="text-brown-2 fill-current mr-3 cursor-pointer" />
             <input
-                className="flex-1 outline-none bg-transparent font-montserrat placeholder-shown:font-light font-medium text-brown-2 italic text-14 tracking-3p leading-none placeholder-yellow-6"
-                type="text"
-                {...props}
+              className="flex-1 outline-none bg-transparent font-montserrat placeholder-shown:font-light font-medium text-brown-2 italic text-14 tracking-3p leading-none placeholder-yellow-6"
+              name="search"
+              type="text"
+              value={search}
+              placeholder="Я шукаю аніме..."
+              onChange={(event) => {
+                if (/^[A-Z\d\sАБВГҐДЕЄЖЗИІЇЙКЛМНОПРСТУФХЦЧШЩЬЮЯ.,!?"'\/$]*$/gi.test(event.target.value) || event.target.value === '') {
+                  handleChange(event)
+                  debouncedSubmitForm()
+                }
+              }}
             />
-        </label>
-    </div>
+          </label>
+        </div>
+      );
+    }}
+  </Formik>
 );
 
 export default SearchInput;
