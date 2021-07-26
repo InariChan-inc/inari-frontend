0a5560f2e7aa5b0b37508b9f1cb863a10546a8a4
Vitalii Shulhan <vitalii.shulhan@gmail.com>
2021-07-27 01:37:19 +0200

    Input: updated implementation

--- molecules/Input/v01/index.tsx
+++ molecules/Input/v01/index.tsx
@@ -1,11 +1,17 @@
-import { FunctionComponent } from "react";
+import {
+  FunctionComponent,
+  JSXElementConstructor,
+  useState,
+  useEffect,
+  useRef,
+} from "react";
 import {
   useField,
 } from 'formik';
-import { JSXElementConstructor } from "react";
 import {
-  Body
-} from '../../../typography';
+  ErrorIcon,
+  Check,
+} from "../../../atoms/icons";
 
 
 export interface InputProps {
@@ -14,26 +20,88 @@ export interface InputProps {
   label: string,
   name: string,
   type?: string,
+  validating?: boolean,
 }
 
 const Input: FunctionComponent<InputProps> = ({
-  disabled = false,
+  disabled,
   Icon,
   label,
   name,
   type = "text",
+  validating,
 }) => {
+  const [focused, setFocused] = useState(false);
 
-  const [field, meta, helpers] = useField(name);
+  const [field, meta, helpers] = useField<string>(name);
 
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    if (field.value) {
+      helpers.setTouched(false);
+    }
+  }, [field.value]);
+  
   return (
-    <div className="relative min-w-[200px] px-6 py-4 rounded-full border">
-      <label className="absolute">
-        <Body type={7}>
-          {label}
-        </Body>
+    <div 
+      className={`relative min-w-[200px] rounded-full ${focused ? 'shadow-form-light' : ''}`} 
+      onClick={() => inputRef.current?.focus()}
+    >
+      {
+        Icon ? (
+          <Icon className={`z-10 absolute -translate-y-1/2 left-6 top-1/2 text-yellow-6 fill-current 
+          ${disabled ? 'cursor-not-allowed' : 'cursor-text'}`} />
+        ) : null
+      }
+      
+      {
+        validating && field.value && meta.touched ? meta.error ? (
+          <ErrorIcon className="text-red-1 fill-current absolute -translate-y-1/2 right-6 top-1/2" />
+        ) : (
+          <Check className="text-green-1 fill-current absolute -translate-y-1/2 right-6 top-1/2" />
+        ) : null
+      }
+      
+
+      <label className={`z-10 select-none pointer-events-none bg-current duration-300 absolute -translate-y-1/2 font-montserrat font-light italic text-14 tracking-3p leading-none 
+                        ${focused || !!field.value ? 'top-0 left-8' : `top-1/2 ${Icon ? 'left-16' : 'left-6'}`} 
+                        ${disabled ? 'text-yellow-5 cursor-not-allowed' : `cursor-text ${validating && field.value && meta.touched? meta.error ? 'text-red-2' : 'text-green-2' : 'text-yellow-6'}`} `}>
+        {label}
       </label>
-      <input className="font-montserrat font-light italic text-14 tracking-3p leading-none" />
+      <div className="relative w-full h-full">
+        <input 
+          ref={inputRef}
+          type={type}
+          name={name}
+          disabled={disabled}
+          className={`px-6 py-4 outline-none font-montserrat font-medium italic text-16 rounded-full 
+                    ${disabled ? 'bg-yellow-1 cursor-not-allowed' : 'bg-transparent'} ${Icon ? 'pl-16' : ''} 
+                    ${validating && field.value && meta.touched && meta.error ? 'text-red-2' : ''} 
+                    ${validating ? 'pr-16' : ''}`}
+          onFocus={() => setFocused(true)}
+          onBlur={(event) => {
+            field.onBlur(event);
+            setFocused(false);
+          }}
+          value={field.value}
+          onChange={event => {
+            field.onChange(event);
+          }}
+        />
+        <fieldset className={`select-none pointer-events-none absolute left-0 right-0 bottom-0 top-[-6px] px-7 border 
+                            ${validating && field.value && meta.touched ? meta.error ? 'border-red-2' : 'border-green-2' : 'border-yellow-4'} rounded-full 
+                            ${!disabled && !meta.error ? 'hover:border-yellow-6' : ''}`}
+        >
+          <legend className={`transition-all duration-300 block select-none pointer-events-none font-montserrat font-light italic text-14 leading-none text-transparent 
+                            ${focused || !!field.value || (disabled !== undefined && !disabled) ? 'px-1 w-[100px]' : 'w-0'}`}
+          >
+            <span>
+              {label}
+            </span>
+          </legend>
+        </fieldset>
+      </div>
     </div>
   );
 };
