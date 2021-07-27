a276c50745a8ee55b8d058c518d35ae2af0b1db3
Vitalii Shulhan <vitalii.shulhan@gmail.com>
2021-07-27 16:11:34 +0200

    Added className prop into Body, Button and Headline typographies

--- typography/v01/Body.tsx
+++ typography/v01/Body.tsx
@@ -1,26 +1,31 @@
+import { ReactNode } from 'react';
 import { FunctionComponent } from 'react';
-
 
 type BodyType = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8;
 
 interface BodyProps {
-    type: BodyType,
-    children: string,
+  type: BodyType,
+  children: ReactNode,
+  className?: string,
 }
 
 const bodyStyles: {[type in BodyType]: string} = {
-    1: 'font-montserrat text-24 tracking-normal leading-none',
-    2: 'font-montserrat text-18 tracking-normal leading-none',
-    3: 'font-montserrat font-bold text-18 tracking-normal leading-none',
-    4: 'font-montserrat font-medium text-16 tracking-normal leading-140p',
-    5: 'font-montserrat font-bold text-16 tracking-normal leading-140p',
-    6: 'font-montserrat font-semibold text-14 tracking-normal leading-none',
-    7: 'font-montserrat font-light italic text-14 tracking-3p leading-none',
-    8: 'font-montserrat font-medium text-14 tracking-normal leading-none',
+  1: 'font-montserrat text-24 tracking-normal leading-none',
+  2: 'font-montserrat text-18 tracking-normal leading-none',
+  3: 'font-montserrat font-bold text-18 tracking-normal leading-none',
+  4: 'font-montserrat font-medium text-16 tracking-normal leading-140p',
+  5: 'font-montserrat font-bold text-16 tracking-normal leading-140p',
+  6: 'font-montserrat font-semibold text-14 tracking-normal leading-none',
+  7: 'font-montserrat font-light italic text-14 tracking-3p leading-none',
+  8: 'font-montserrat font-medium text-14 tracking-normal leading-none',
 }
 
-const Body: FunctionComponent<BodyProps> = ({type, children}) => (
-    <p className={bodyStyles[type]}>{children}</p>
+const Body: FunctionComponent<BodyProps> = ({
+  type, 
+  children,
+  className = '',
+}) => (
+  <p className={`${bodyStyles[type]} ${className}`}>{children}</p>
 )
 
 
--- typography/v01/Headline.tsx
+++ typography/v01/Headline.tsx
@@ -4,21 +4,26 @@ import { FunctionComponent } from 'react';
 type HeadlineType =  1 | 2 | 3 | 4;
 
 interface HeadlineProps {
+    className?: string
     type: HeadlineType,
     children: string,
 }
 
-const HeaderComponents: {[type in HeadlineType]: React.FunctionComponent<{children: string}>} = {
-    1: ({children}) => <h1 className="font-montserrat font-bold text-48 tracking-normal leading-none">{children}</h1>,
-    2: ({children}) => <h2 className="font-montserrat font-bold text-36 tracking-normal leading-none">{children}</h2>,
-    3: ({children}) => <h3 className="font-montserrat font-bold text-28 tracking-normal leading-none">{children}</h3>,
-    4: ({children}) => <h4 className="font-montserrat font-bold text-20 tracking-normal leading-none">{children}</h4>,
+const HeaderComponents: {[type in HeadlineType]: React.FunctionComponent<{children: string, className?: string}>} = {
+    1: ({children, className = ''}) => <h1 className={`font-montserrat font-bold text-48 tracking-normal leading-none ${className}`}>{children}</h1>,
+    2: ({children, className = ''}) => <h2 className={`font-montserrat font-bold text-36 tracking-normal leading-none ${className}`}>{children}</h2>,
+    3: ({children, className = ''}) => <h3 className={`font-montserrat font-bold text-28 tracking-normal leading-none ${className}`}>{children}</h3>,
+    4: ({children, className = ''}) => <h4 className={`font-montserrat font-bold text-20 tracking-normal leading-none ${className}`}>{children}</h4>,
 };
 
-const Headline: FunctionComponent<HeadlineProps> = ({type, children}) => {
-    const HeaderComponent = HeaderComponents[type];
+const Headline: FunctionComponent<HeadlineProps> = ({
+  className = '',
+  type, 
+  children,
+}) => {
+  const HeaderComponent = HeaderComponents[type];
 
-    return <HeaderComponent>{children}</HeaderComponent>
+  return <HeaderComponent className={className}>{children}</HeaderComponent>
 };
 
 
