7f14bfbb93b7c89b69ac93d3eb37ea1b8b516950
Vitalii Shulhan <vitalii.shulhan@gmail.com>
2021-07-27 16:10:24 +0200

    Added Key icon

--- atoms/icons/index.ts
+++ atoms/icons/index.ts
@@ -14,6 +14,7 @@ import Filter from "./Filter";
 import Email from "./Email";
 import ErrorIcon from "./Error";
 import Check from "./Check";
+import Key from './Key';
 
 export {
     Search,
@@ -32,4 +33,5 @@ export {
     Email,
     ErrorIcon,
     Check,
+    Key,
 };
