319b749056e769fd9babd49b9287cf29ebae26e3
Vitalii Shulhan <vitalii.shulhan@gmail.com>
2021-08-01 14:39:51 +0200

    Fixed typography: removed ButtonTextColors type exporting, changed button type implementation for Button typography

--- typography/v01/Button.tsx
+++ typography/v01/Button.tsx
@@ -1,17 +1,16 @@
 import { FunctionComponent } from "react";
 
-type ButtonType = 1 | 2 | 3;
 
-interface ButtonProps {
-  type: ButtonType,
-  children: string,
-  className?: string,
-}
-
-const buttonStyles: {[type in ButtonType]: string} = {
+const buttonStyles = {
   1: 'font-montserrat font-medium text-18 tracking-3p leading-none"',
   2: 'font-montserrat font-bold text-13 tracking-3p leading-none"',
   3: 'font-montserrat text-16 tracking-1p leading-none"',
+}
+
+interface ButtonProps {
+  type: keyof typeof buttonStyles,
+  children: string,
+  className?: string,
 }
 
 const Button: FunctionComponent<ButtonProps> = ({
