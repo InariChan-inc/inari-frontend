0b52ae654e5d54ef260a5014ba27d8f2e5bc18aa
Vitalii Shulhan <vitalii.shulhan@gmail.com>
2021-08-01 14:41:05 +0200

    Button: added ref forwarding, Input: set body 11 typography for all helper content

--- molecules/Button/v01/index.tsx
+++ molecules/Button/v01/index.tsx
@@ -1,6 +1,5 @@
-import { CSSProperties } from "react";
-import { FunctionComponent, JSXElementConstructor } from "react";
-import { Button as Text, ButtonTextColors } from '../../../typography';
+import React, { CSSProperties } from "react";
+import { Button as Text } from '../../../typography';
 
 const types = {
   1: 'text-white disabled:text-yellow-5 duration-300 disabled:cursor-not-allowed bg-brown-2 hover:bg-brown-1 disabled:bg-yellow-2',
@@ -16,36 +15,33 @@ interface ButtonProps {
     disabled?: boolean
 }
 
-const Button: FunctionComponent<ButtonProps> = ({
-    onClick,
-    children = '',
-    icon,
-    style,
-    className = '',
-    disabled,
-    type = 1,
-    buttonType = 'button',
-}) => {
-
-  return (
-    <button 
-      type={buttonType}
-      disabled={disabled}
-      className={`flex items-center px-6 py-3 rounded-full ${types[type]} ${className}`}
-      style={style}
-      onClick={onClick}
+const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(({
+  onClick,
+  children = '',
+  icon,
+  style,
+  className = '',
+  disabled,
+  type = 1,
+  buttonType = 'button',
+}, ref) => (
+  <button
+    ref={ref}
+    type={buttonType}
+    disabled={disabled}
+    className={`flex items-center px-6 py-3 rounded-full ${types[type]} ${className}`}
+    style={style}
+    onClick={onClick}
+  >
+    {icon}
+    <Text
+      className={`${icon ? 'pl-2' : ''}`}
+      type={1}
     >
-      {icon}
-      <div className={`${icon ? 'pl-2' : ''}`}>
-        <Text 
-          type={1}
-        >
-          {children}
-        </Text>
-      </div>
-    </button>
-  );
-};
+      {children}
+    </Text>
+  </button>
+));
 
 
 export default Button;
