d8e703a26ca421ba4a3fa28094edbdc1ff80e57b
Vitalii Shulhan <vitalii.shulhan@gmail.com>
2021-08-11 17:43:56 +0200

    Added selector if the user is logged in

--- redux/selectors/token.ts
+++ redux/selectors/token.ts
@@ -4,4 +4,5 @@ import { GeneralToken } from "../../common/graphql/interfaces";
 const selectSelf = ({ token }: { token: GeneralToken}) => token;
 
 export const getToken = createSelector(selectSelf, state => ({token: state.token, exp: state.tokenExp}));
-export const getRefreshToken = createSelector(selectSelf, state => ({token: state.refreshToken, exp: state.refreshTokenExp}))
+export const getRefreshToken = createSelector(selectSelf, state => ({token: state.refreshToken, exp: state.refreshTokenExp}));
+export const isUserLoggedIn = createSelector(selectSelf, state => !!state.token);
