f3eb63d05738b2345443f8f10d0371c8ea8ad932
Vitalii Shulhan <vitalii.shulhan@gmail.com>
2021-08-12 17:41:04 +0200

    Added 4th type of button typography

--- typography/v01/Button.tsx
+++ typography/v01/Button.tsx
@@ -2,9 +2,10 @@ import { FunctionComponent } from "react";
 
 
 const buttonStyles = {
-  1: 'font-montserrat font-medium text-18 tracking-3p leading-none"',
-  2: 'font-montserrat font-bold text-13 tracking-3p leading-none"',
-  3: 'font-montserrat text-16 tracking-1p leading-none"',
+  1: 'font-montserrat font-medium text-18 tracking-3p leading-none',
+  2: 'font-montserrat font-bold text-13 tracking-3p leading-none',
+  3: 'font-montserrat text-16 tracking-1p leading-none',
+  4: 'font-montserrat font-semibold, text-18'
 }
 
 interface ButtonProps {
