68ad40850d20b72f7fc14cbf599aeb8812e2482c
Vitalii Shulhan <vitalii.shulhan@gmail.com>
2021-08-12 17:36:36 +0200

    Fixed truncateBySymbols implementation

--- utils/index.ts
+++ utils/index.ts
@@ -1,5 +1,5 @@
 export const truncateByWords: (text: string, wordsLimit: number) => string = (text, wordsLimit) => text.split(' ').length <= wordsLimit ? text : text.split(' ').slice(0, wordsLimit).join(' ') + '...';
-export const truncateBySymbols: (text: string, symbolsLimit: number) => string = (text, symbolsLimit) => text.slice(0, symbolsLimit) + '...';
+export const truncateBySymbols: (text: string, symbolsLimit: number) => string = (text, symbolsLimit) => text.length <= symbolsLimit ? text : text.slice(0, symbolsLimit) + '...';
 export const sleep: (callback: () => any, timeout: number) => Promise<any> = (callback, timeout) => new Promise(res => setTimeout(res, timeout)).then(callback);
 export const getFirstLetters: (str: string, amount: number) => string = (str, amount) => {
   const strArray = str.split(' ');
