dbea763365322f490d332e98634e839391e3b351
Vitalii Shulhan <vitalii.shulhan@gmail.com>
2021-08-12 17:40:25 +0200

    Implemented AccountControl component

--- components/Header/v01/index.tsx
+++ components/Header/v01/index.tsx
@@ -3,10 +3,13 @@ import {
 } from "react";
 import { useSelector } from "react-redux";
 import { isFocused } from "../../../redux/selectors/headerSearch";
-
+import { isUserLoggedIn } from "../../../redux/selectors/token";
 import { Sun } from '../../../atoms/icons';
 import { SearchInput } from '../../../molecules';
-import { Authorization } from '../../../organizms';
+import {
+  Authorization,
+  AccountControl
+} from '../../../organizms';
 
 interface HeaderProps {
 }
@@ -14,24 +17,32 @@ interface HeaderProps {
 const Header: FunctionComponent<HeaderProps> = ({
 }) => {
   const isSearchFocused = useSelector(isFocused);
+  const isLoggedIn = useSelector(isUserLoggedIn);
 
   return (
     <div
-      className={`fixed top-0 right-0 left-[132px] z-[5001] h-[80px] flex items-center justify-between py-4 px-[60px] bg-white border-b border-yellow-1 before:absolute before:top-0 before:left-[-1px] before:bottom-0 before:w-[1px] before:bg-white`}>
+      className={`fixed top-0 right-0 left-[132px] z-[5001] h-[80px] flex items-center justify-between px-[60px] bg-white border-b border-yellow-1 before:absolute before:top-0 before:left-[-1px] before:bottom-0 before:w-[1px] before:bg-white`}>
         <SearchInput 
             placeholder="Шукаю аніме..."
             onSearch={searchValue => console.log('SEARCH: ', searchValue)}
             onSubmit={searchValue => console.log('SUBMITTED: ', searchValue)}
         />
         
-        <div className="flex items-center">
-          <Sun 
-            size={36}
-            className="text-brown-2 dark:text-white fill-current cursor-pointer mr-8" 
-            onClick={() => console.log('Theme switched!')}
-          />
-          <Authorization />
-        </div>
+        {
+          isLoggedIn ? (
+            <AccountControl />
+          ) : (
+            <div className="flex items-center h-full">
+              <Sun 
+                size={36}
+                className="text-brown-2 dark:text-white fill-current cursor-pointer mr-8" 
+                onClick={() => console.log('Theme switched!')}
+              />
+              <Authorization />
+            </div>
+          )
+        }
+        
         <div className={`duration-300 fixed z-[9998] top-0 left-0 right-0 bottom-0 bg-black opacity-60 ${isSearchFocused ? 'visible' : 'opacity-0 invisible'}`} />
     </div>
   );
--- organizms/AccountControl/v01/index.tsx
+++ organizms/AccountControl/v01/index.tsx
@@ -1,4 +1,39 @@
-import { FunctionComponent } from 'react';
+import {
+  FunctionComponent,
+  useEffect,
+  useRef
+} from 'react';
+import {
+  useSelector,
+  useDispatch
+} from 'react-redux';
+import { clearUser } from '../../../redux/actions/user';
+import { deleteAll } from '../../../redux/actions/token';
+import { truncateBySymbols } from '../../../utils';
+import {
+  getName,
+  getRole,
+  getAvatar
+} from '../../../redux/selectors/user';
+import {
+  ArrowUp,
+  ArrowDown,
+  LogOut
+} from '../../../atoms/icons';
+import {
+  Avatar,
+  Link
+} from '../../../atoms';
+import {
+  Body,
+  Button as ButtonText
+} from '../../../typography';
+import useDropMenuOutsideClick from '../../../hooks/useDropMenuOutsideClick';
+import menuItems from './menuItems';
+
+const getRandom: (min: number, max: number) => number = (min, max) => Math.floor(Math.random()*(max - min)) + min;
+
+const getHSL = () => `hsl(${getRandom(25, 45)}, ${getRandom(50, 100)}%, ${getRandom(30,60)}%)`;
 
 
 export interface AccountControlProps {
@@ -8,11 +43,91 @@ export interface AccountControlProps {
 const AccountControl: FunctionComponent<AccountControlProps> = ({
 
 }) => {
+  const dispatch = useDispatch();
 
+  const name = useSelector(getName);
+
+  const color = useRef<string>(getHSL());
+
+  const role = useSelector(getRole);
+  const avatar = useSelector(getAvatar);
+
+  const rootRef = useRef<HTMLDivElement>(null);
+  const menuRootRef = useRef<HTMLDivElement>(null);
+
+  const [handleClick, open] = useDropMenuOutsideClick(rootRef, menuRootRef);
+
+  useEffect(() => {
+    document.addEventListener('click', handleClick);
+
+    return () => {
+      document.removeEventListener('click', handleClick);
+    }
+  })
 
   return (
-    <div>
-      ACCOUNT CONTROL
+    <div className="flex items-center" onBlur={() => console.log('BLUR')}>
+      <div
+        ref={rootRef}
+        className={`relative max-w-[300px] flex justify-end items-center h-full px-6 py-4 mr-8 cursor-pointer border hover:bg-yellow-7 hover:border-yellow-1 ${open ? 'border-yellow-1 bg-yellow-7' : 'border-transparent'}`}
+      >
+        <div className="flex flex-col justify-center items-end">
+          <Body
+            className="text-brown-2"
+            type={5}
+          >
+            {truncateBySymbols(name !== '' ? name : 'Very cool nickname', 16)}
+          </Body>
+          <Body
+            className="text-brown-2"
+            type={8}
+          >
+            {role ? role.name : 'Admin'}
+          </Body>
+        </div>
+        <Avatar
+          className="w-12 h-12 ml-4 mr-2"
+          name={name !== '' ? name : 'Very cool nickname'}
+          color={getHSL()}
+        />
+
+        {
+          open ? (
+            <ArrowUp className="text-brown-2 fill-current" />
+          ) : (
+            <ArrowDown className="text-brown-2 fill-current" />
+          )
+        }
+        
+
+        <div
+          ref={menuRootRef}
+          className={`absolute min-w-[300px] border border-yellow-1 bg-white translate-y-full bottom-0 right-0 py-4 ${open ? 'block' : 'hidden'}`}
+        >
+          {
+            menuItems.map(({
+              Icon,
+              path,
+              title
+            }) => (
+              <Link href={path}>
+                <div className="w-full px-8 py-4 flex items-center border border-transparent hover:bg-yellow-7 hover:border-yellow-1">
+                  <Icon className="mr-6" />
+                  <ButtonText type={4}>{title}</ButtonText>
+                </div>
+              </Link>
+            ))
+          }
+        </div>
+      </div>
+      <LogOut
+        className="text-brown-2 fill-current cursor-pointer"
+        size={32}
+        onClick={() => {
+          dispatch(clearUser());
+          dispatch(deleteAll());
+        }}
+      />
     </div>
   );
 };
