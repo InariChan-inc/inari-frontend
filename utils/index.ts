f913abcdfc732b0e0206b195fd539562509b1749
Vitalii Shulhan <vitalii.shulhan@gmail.com>
2021-08-15 22:44:14 +0200

    Add type validation for utils that operate on strings

--- utils/index.ts
+++ utils/index.ts
@@ -1,7 +1,10 @@
-export const truncateByWords: (text: string, wordsLimit: number) => string = (text, wordsLimit) => text.split(' ').length <= wordsLimit ? text : text.split(' ').slice(0, wordsLimit).join(' ') + '...';
-export const truncateBySymbols: (text: string, symbolsLimit: number) => string = (text, symbolsLimit) => text.length <= symbolsLimit ? text : text.slice(0, symbolsLimit) + '...';
+export const truncateByWords: (text: string, wordsLimit: number) => string = (text, wordsLimit) => typeof text === 'string' ? text.split(' ').length <= wordsLimit ? text : text.split(' ').slice(0, wordsLimit).join(' ') + '...' : '';
+export const truncateBySymbols: (text: string, symbolsLimit: number) => string = (text, symbolsLimit) => typeof text === 'string' ? text.length <= symbolsLimit ? text : text.slice(0, symbolsLimit) + '...' : '';
 export const sleep: (callback: () => any, timeout: number) => Promise<any> = (callback, timeout) => new Promise(res => setTimeout(res, timeout)).then(callback);
 export const getFirstLetters: (str: string, amount: number) => string = (str, amount) => {
+  if (typeof str !== 'string')
+    return '';
+
   const strArray = str.split(' ');
 
   if (strArray.length === 1) {
