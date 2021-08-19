af06e8f30cbfe795af7d04146039d79411c79594
Vitalii Shulhan <vitalii.shulhan@gmail.com>
2021-08-20 01:19:37 +0200

    Home page: created queries for top anime month, last added and last updated sections of anime, passing props into the corresponding sliders

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,33 +1,96 @@
 import Head from 'next/head';
-
 import {
   ImageSlider,
   AnimeSlider,
 } from '../components';
-
 import {
-  Button
+  Button,
+  AnimeCardProps
 } from '../molecules';
-
 import {
   useQuery,
   gql,
 } from '@apollo/client';
-
 import {
   Link
 } from '../atoms';
 
+
 export default function Home() {
 
-  const {data} = useQuery<{baners: {image: {path: string}}[]}>(gql`
-    {
-      baners {
-        image {
-          path
+  const { 
+    data: topAnimeMonthData,
+    loading: topAnimeMonthLoading,
+    error: topAnimeMonthError
+  } = useQuery<{ topAnimeMonth: AnimeCardProps[] }>(
+    gql`
+      {
+        topAnimeMonth {
+          name
+          poster {
+            path
+            pathResized
+          }
+          currentCountEpisodes
+          countEpisodes
+          format
         }
       }
-    }
+  `);
+
+  const {
+    data: lastUpdatedAnimeData,
+    loading: lastUpdatedAnimeLoading,
+    error: lastUpdatedAnimeError
+  } = useQuery<{ lastUpdatedAnime: AnimeCardProps[] }>(
+    gql`
+      {
+        lastUpdatedAnime {
+          name
+          poster {
+            path
+            pathResized
+          }
+          currentCountEpisodes
+          countEpisodes
+          format
+        }
+      }
+  `);
+
+  const {
+    data: lastAddedAnimeData,
+    loading: lastAddedAnimeLoading,
+    error: lastAddedAnimeError
+  } = useQuery<{ lastAddedAnime: AnimeCardProps[] }>(
+    gql`
+      {
+        lastAddedAnime {
+          name
+          poster {
+            path
+            pathResized
+          }
+          currentCountEpisodes
+          countEpisodes
+          format
+        }
+      }
+  `);
+
+  const { 
+    data: bannerData, 
+    loading: bannerLoading,
+    error: bannerError
+  } = useQuery<{baners: {image: {path: string}}[]}>(
+    gql`
+      {
+        baners {
+          image {
+            path
+          }
+        }
+      }
   `);
 
   return (
@@ -36,27 +99,49 @@ export default function Home() {
         <title>Inari - Головна</title>
       </Head>
 
-        <ImageSlider
-          urls={data && data.baners.map(item => `${process.env.HOST}/${item.image.path}`)}
-        />
-        <AnimeSlider
-          className="my-16"
-          title="Топ місяця"
-        />
-        <AnimeSlider
-          className="my-16"
-          title="Нещодавно оновлені"
-        />
-        <AnimeSlider
-          className="my-16"
-          title="Останні додані"
-          slidesPerColumn={2}
-        />
-        <div className="w-full flex justify-center">
+        {
+          !bannerLoading && !bannerError && bannerData && bannerData.baners.length ? (
+            <ImageSlider
+              urls={bannerData.baners.map(item => `${process.env.HOST}/${item.image.path}`)}
+            />
+          ) : null
+        }
+        
+
+        {
+          !topAnimeMonthLoading && !topAnimeMonthError && topAnimeMonthData ? (
+            <AnimeSlider
+              className="my-16"
+              title="Топ місяця"
+              animes={topAnimeMonthData.topAnimeMonth}
+            />
+          ) : null
+        }       
+        
+        {
+          !lastUpdatedAnimeLoading && !lastUpdatedAnimeError && lastUpdatedAnimeData ? (
+            <AnimeSlider
+              className="my-16"
+              title="Нещодавно оновлені"
+              animes={lastUpdatedAnimeData.lastUpdatedAnime}
+            /> 
+          ) : null
+        }
+
+        {
+          !lastAddedAnimeLoading && !lastAddedAnimeError && lastUpdatedAnimeData ? (
+            <AnimeSlider
+              className="my-16"
+              title="Останні додані"
+              slidesPerColumn={2}
+              animes={lastAddedAnimeData.lastAddedAnime}
+            />
+          ) : null
+        }
+
+       <div className="w-full flex justify-center">
           <Link href="search">
-            <Button
-              className="px-7 py-3"
-            >
+            <Button className="px-7 py-3">
               Перейти до пошуку
             </Button>
           </Link>
