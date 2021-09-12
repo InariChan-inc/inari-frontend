7acb73da3e3a162be0fe83e29458ac028b8c18e7
Vitalii Shulhan <vitalii.shulhan@gmail.com>
2021-09-12 10:47:11 +0200

    Refactored index.ts in atoms/icons

--- atoms/icons/index.ts
+++ atoms/icons/index.ts
@@ -1,53 +1,25 @@
-import Search from "./Search";
-import SearchRounded from "./SearchRounded";
-import Sun from "./Sun";
-import Person from "./Person";
-import Support from "./Support";
-import Cooperation from "./Cooperation";
-import Help from "./Help";
-import Info from "./Info";
-import ArrowLeft from "./ArrowLeft";
-import ArrowRight from "./ArrowRight";
-import ArrowDown from "./ArrowDown";
-import ArrowUp from "./ArrowUp";
-import Filter from "./Filter";
-import Email from "./Email";
-import ErrorIcon from "./Error";
-import Check from "./Check";
-import Key from './Key';
-import Visibility from "./Visibility";
-import Copyright from "./Copyright";
-import LogOut from "./LogOut";
-import People from "./People";
-import PieChart from "./PieChart";
-import Settings from "./Settings";
-import AccountCircle from "./AccountCircle";
-import Bookmark from "./Bookmark";
-
-export {
-    Search,
-    SearchRounded,
-    Sun,
-    Person,
-    Support,
-    Cooperation,
-    Help,
-    Info,
-    ArrowLeft,
-    ArrowRight,
-    ArrowDown,
-    ArrowUp,
-    Filter,
-    Email,
-    ErrorIcon,
-    Check,
-    Key,
-    Visibility,
-    Copyright,
-    LogOut,
-    People,
-    PieChart,
-    Settings,
-    AccountCircle,
-    Bookmark
-};
+export { default as Search } from "./Search";
+export { default as SearchRounded } from "./SearchRounded";
+export { default as Sun } from "./Sun";
+export { default as Person } from "./Person";
+export { default as Support } from "./Support";
+export { default as Cooperation } from "./Cooperation";
+export { default as Help } from "./Help";
+export { default as Info } from "./Info";
+export { default as ArrowLeft } from "./ArrowLeft";
+export { default as ArrowRight } from "./ArrowRight";
+export { default as ArrowDown } from "./ArrowDown";
+export { default as ArrowUp } from "./ArrowUp";
+export { default as Filter } from "./Filter";
+export { default as Email } from "./Email";
+export { default as ErrorIcon } from "./Error";
+export { default as Check } from "./Check";
+export { default as Key } from './Key';
+export { default as Visibility } from "./Visibility";
+export { default as Copyright } from "./Copyright";
+export { default as LogOut } from "./LogOut";
+export { default as People } from "./People";
+export { default as PieChart } from "./PieChart";
+export { default as Settings } from "./Settings";
+export { default as AccountCircle } from "./AccountCircle";
+export { default as Bookmark } from "./Bookmark";
