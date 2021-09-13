69bfb6fc3469f6d10667a2709b237816e4b89c1a
Vitalii Shulhan <vitalii.shulhan@gmail.com>
2021-09-13 17:34:37 +0200

    Refactored Input

--- molecules/Input/v01/index.tsx
+++ molecules/Input/v01/index.tsx
@@ -9,18 +9,26 @@ import {
 import {
   useField,
 } from 'formik';
+import { BaseIconProps } from "@atoms/interfaces";
+import { Body } from "@typography";
 import {
-  ErrorIcon,
-  Check,
-  Visibility,
-} from "../../../atoms/icons";
-import { Body } from "../../../typography";
-
+  InputContainer,
+  FieldContainer,
+  IconWrapper,
+  StyledErrorIcon,
+  StyledCheckIcon,
+  StyledVisibilityIcon,
+  Label,
+  StyledInput,
+  Fieldset,
+  Legend,
+  Helper,
+} from './styles';
 
 export interface InputProps {
   className?: string,
   disabled?: boolean,
-  Icon?: JSXElementConstructor<{ className: string }>
+  Icon?: JSXElementConstructor<BaseIconProps>
   label: string,
   name: string,
   type?: string,
@@ -43,7 +51,6 @@ const Input: FunctionComponent<InputProps> = ({
   focusedOnStart
 }) => {
 
-  const commonIconClassNames = 'fill-current absolute -translate-y-1/2 top-1/2';
   const rightIconPositionClassName = type === 'password' ? 'right-14' : 'right-6';
 
   const [passwordInputType, setPasswordInputType] = useState('password');
@@ -77,30 +84,30 @@ const Input: FunctionComponent<InputProps> = ({
   }, [field.value]);
   
   return (
-    <div className={`w-full ${className}`}>
-      <div
-        className={`relative rounded-full h-14 ${focused ? 'shadow-form-light' : ''}`}
+    <InputContainer>
+      <FieldContainer
+        focused={focused}
         onClick={() => {inputRef.current?.focus()}}
       >
       {
         Icon ? (
-          <Icon className={`z-10 text-yellow-6 left-6 ${commonIconClassNames} ${disabled ? 'cursor-not-allowed' : 'cursor-text'}`}
-          />
+          <IconWrapper disabled={disabled}>
+            <Icon />
+          </IconWrapper>
         ) : null
       }
       
       {
         error !== undefined && meta.touched && !isThisInputBeingValidated ? error ? (
-          <ErrorIcon className={`text-red-1 cursor-text ${rightIconPositionClassName} ${commonIconClassNames}`} />
+          <StyledErrorIcon inputType={type} />
         ) : (
-          <Check className={`text-green-1 cursor-text ${rightIconPositionClassName} ${commonIconClassNames}`} />
+          <StyledCheckIcon inputType={type} />
         ) : null
       }
       
       {
         type === 'password' ? (
-          <Visibility 
-            className={`cursor-pointer ${commonIconClassNames} text-yellow-6 right-6`} 
+          <StyledVisibilityIcon
             visible={passwordInputType === 'text'}
             onClick={event => {
               event.stopPropagation();
@@ -110,23 +117,30 @@ const Input: FunctionComponent<InputProps> = ({
         ) : null
       }
 
-      <label 
-        className={`z-10 select-none pointer-events-none bg-current duration-300 absolute -translate-y-1/2 font-montserrat font-light italic text-14 tracking-3p leading-none
-                  ${focused || !!field.value ? 'top-0 left-8' : `top-1/2 ${Icon ? 'left-16' : 'left-6'}`} 
-                  ${disabled ? 'text-yellow-5' : `${error !== undefined && meta.touched && !isThisInputBeingValidated ? error ? 'text-red-2' : 'text-green-2' : 'text-yellow-6'}`} `}
-        >
+      <Label
+        as="label"
+        type={7}
+        disabled={disabled}
+        error={error}
+        focused={focused}
+        isFieldNotEmpty={!!field.value}
+        isIcon={!!Icon}
+        isThisInputBeingValidated={isThisInputBeingValidated}
+        isTouched={meta.touched}
+      >
         {label}
-      </label>
+      </Label>
 
-        <input 
+        <StyledInput 
           ref={inputRef}
           type={type === 'password' ? passwordInputType : type}
           name={name}
           disabled={disabled}
-          className={`w-full px-6 py-4 outline-none font-montserrat font-medium italic text-16 rounded-full bg-transparent disabled:bg-yellow-1 cursor-text disabled:cursor-not-allowed
-                    ${Icon ? 'pl-16' : ''} 
-                    ${error !== undefined && meta.touched && !isThisInputBeingValidated && error ? 'text-red-2' : ''} 
-                    ${type === 'password' ? 'pr-24' : error !== undefined ? 'pr-16'  : ''}`}
+          error={error}
+          isFieldNotEmpty={!!field.value}
+          isIcon={!!Icon}
+          isThisInputBeingValidated={isThisInputBeingValidated}
+          isTouched={meta.touched}
           onFocus={() => setFocused(true)}
           onBlur={(event) => {
             field.onBlur(event);
@@ -141,31 +155,41 @@ const Input: FunctionComponent<InputProps> = ({
             }
           }}
         />
-        <fieldset 
-          className={`rounded-full select-none pointer-events-none absolute left-0 right-0 bottom-0 top-[-6px] px-7 border 
-                    ${error !== undefined && !isThisInputBeingValidated && meta.touched ? meta.error ? 'border-red-2' : 'border-green-2' : 'border-yellow-4'}
-                    ${!disabled && !meta.error ? 'hover:border-yellow-6' : ''}`}
+        <Fieldset
+          disabled={disabled}
+          error={error}
+          metaError={meta.error}
+          isTouched={meta.touched}
+          isThisInputBeingValidated={isThisInputBeingValidated}
         >
-          <legend 
-            className={`border-separate transition-all duration-200 block select-none pointer-events-none font-montserrat font-light italic text-14 leading-none text-transparent 
-                      ${focused || !!field.value || (disabled !== undefined && !disabled) ? 'px-1 max-w-full' : 'max-w-0'}`}
+          <Legend
+            as="legend"
+            type={7}
+            disabled={disabled}
+            focused={focused}
+            isFieldNotEmpty={!!field.value}
+
           >
             <span>
               {label}
             </span>
-          </legend>
-        </fieldset>
-      </div>
+          </Legend>
+        </Fieldset>
+      </FieldContainer>
       {
         helper ? (
-          <div className={`select-none px-6 py-2 ${error !== undefined && meta.touched && !isThisInputBeingValidated ? error ? 'text-red-2' : 'text-green-2' : 'text-yellow-6'}`}>
-            <Body type={11}>
-              {helper}
-            </Body>
-          </div>
+          <Helper
+            type={11}
+            error={error}
+            isTouched={meta.touched}
+            isThisInputBeingValidated={isThisInputBeingValidated}
+
+          >
+            {helper}
+          </Helper>
         ) : null
       }
-    </div>
+    </InputContainer>
   );
 };
 
---
+++ molecules/Input/v01/styles.tsx
@@ -0,0 +1,163 @@
+import styled, { css } from "styled-components";
+import { Body } from "@typography";
+import {
+  ErrorIcon,
+  Check,
+  Visibility,
+} from '@icons';
+
+interface TrackProps {
+  disabled: boolean,
+  error?: boolean,
+  metaError?: string,
+  focused: boolean,
+  isFieldNotEmpty: boolean,
+  isIcon: boolean,
+  isThisInputBeingValidated: boolean,
+  isTouched: boolean,
+}
+
+
+const commonIconStyles = css`
+  position: absolute;
+  transform: translateY(-50%);
+  top: 50%;
+`;
+
+const getIconRightPosition = (inputType: string) => css`right: ${inputType === 'password' ? 56 : 24}px;`;
+
+export const InputContainer = styled.div`
+  width: 100%;
+`;
+
+export const FieldContainer = styled.div<{ focused: boolean }>`
+  position: relative;
+  border-radius: 9999px;
+  height: 56px;
+  box-shadow: ${props => props.focused ? `0 4px 8px 0 ${props.theme.colors["yellow-1"]}` : ''};
+`;
+
+export const IconWrapper = styled.div<{ disabled: boolean }>`
+  ${commonIconStyles}
+
+  z-index: 10;
+  color: ${props => props.theme.colors["yellow-6"]};
+  cursor: ${props => props.disabled ? 'not-allowed' : 'text'};
+`;
+
+export const StyledErrorIcon = styled(ErrorIcon) <{ inputType: string }>`
+  ${commonIconStyles}
+  ${props => getIconRightPosition(props.inputType)}
+
+  color: ${props => props.theme.colors["red-1"]};
+  cursor: text;
+`;
+
+export const StyledCheckIcon = styled(Check) <{ inputType: string }>`
+  ${commonIconStyles}
+  ${props => getIconRightPosition(props.inputType)}
+
+  color: ${props => props.theme.colors["green-1"]};
+  cursor: text;
+`;
+
+export const StyledVisibilityIcon = styled(Visibility)`
+  ${commonIconStyles}
+
+  right: 24px;
+  cursor: pointer;
+  color: ${props => props.theme.colors["yellow-6"]};
+`;
+
+export const Label = styled(Body)<Omit<TrackProps, 'metaError'>>`
+    position: absolute;
+    top: ${({ focused, isFieldNotEmpty }) => focused || isFieldNotEmpty ? 0 : '50%'};
+    left: ${({ focused, isFieldNotEmpty, isIcon }) => focused || isFieldNotEmpty ? '32px' : isIcon ? '64px' : '24px'};
+    transform: translateY(-50%);
+    z-index: 10;
+
+    user-select: none;
+    pointer-events: none;
+    transition-duration: 300ms;
+
+    color: ${({
+      disabled,
+      error,
+      isTouched,
+      isThisInputBeingValidated,
+      theme: { colors },
+    }) => disabled ? colors["yellow-5"] :
+          error !== undefined && isTouched && !isThisInputBeingValidated ? error ? colors["red-1"] :
+            colors['green-2'] :
+          colors['yellow-6']
+    };
+`;
+
+export const StyledInput = styled.input<Omit<TrackProps, 'disabled' | 'focused' | 'metaError'>>`
+  width: 100%;
+  padding: 16px 24px;
+  padding-left: ${({isIcon}) => isIcon ? '64px' : ''};
+  padding-right: ${({ type, error }) => type === 'password' ? '96px' : error ? '64px' : ''};
+  outline: none;
+  font-family: ${props => props.theme.font.family.montserrat};
+  font-weight: ${props => props.theme.font.weight.medium};
+  font-style: italic;
+  font-size: 16px;
+  border-radius: 9999px;
+  background-color: transparent;
+  cursor: text;
+  color: ${({
+    error,
+    isTouched,
+    isThisInputBeingValidated,
+    theme: { colors },
+  }) => error && isTouched && !isThisInputBeingValidated ? colors["red-1"] : ''};
+
+  :disabled {
+    background-color: ${props => props.theme.colors["yellow-1"]};
+    cursor: not-allowed;
+  }
+`;
+
+export const Fieldset = styled.fieldset<Omit<TrackProps, 'disabled' | 'focused' | 'isFieldNotEmpty' | 'isIcon'>>`
+  position: absolute;
+  top: -6px;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  padding: 0 28px;
+  border-radius: 9999px;
+  border-width: 1px;
+  border-color: ${({error, isThisInputBeingValidated, isTouched, metaError, theme: { colors }}) => error !== undefined && !isThisInputBeingValidated && isTouched ? metaError ? colors["red-2"] : colors["green-2"] : colors["yellow-4"]};
+  
+  user-select: none;
+  pointer-events: none;
+
+  :not(:disabled)::hover {
+    border-color: ${({ metaError, theme: { colors } }) => metaError ? colors["yellow-6"] : ''};
+  }
+`;
+
+export const Legend = styled(Body)<Omit<TrackProps, 'error' | 'metaError' | 'isIcon' | 'isThisInputBeingValidated' | 'isTouched'>>`
+  display: block;
+  color: transparent;
+
+  transition-property: all;
+  transition-duration: 200ms;
+
+  user-select: none;
+  pointer-events: none;
+
+  ${({ focused, isFieldNotEmpty, disabled }) => focused || isFieldNotEmpty || (disabled === false) ? css`
+    padding-left: 4px;
+    padding-right: 4px;
+    max-width: 100%;
+  ` : css`max-width: 0;`}
+`;
+
+export const Helper = styled(Body)<Omit<TrackProps, 'disabled' | 'metaError' | 'focused' | 'isFieldNotEmpty' | 'isIcon'>>`
+  user-select: none;
+  padding: 8px 24px;
+
+  color: ${({ error, isTouched, isThisInputBeingValidated, theme: { colors } }) => error !== undefined && isTouched && !isThisInputBeingValidated ? error ? colors["red-2"] : colors["green-2"] : colors["yellow-6"]};
+`;
