895ce7d316e54af55ea42e4451836ec65e915cee
Vitalii Shulhan <vitalii.shulhan@gmail.com>
2021-10-07 23:42:21 +0200

    Refactored tabs in Avatar component, added size and style optional prop

--- atoms/Avatar/v01/styles.tsx
+++ atoms/Avatar/v01/styles.tsx
@@ -1,20 +1,22 @@
 import styled from "styled-components";
 import { Body } from '@typography';
+import { TStringOrNumber } from "@common/types";
 
-export const AvatarContainer = styled.div<{
-    backgroundImage: string,
-    backgroundColor: string,
-}>`
-   border-radius: 9999px;
-   background-image: ${props => props.backgroundImage};
-   background-color: ${props => props.backgroundColor};
-   user-select: none;
+export const AvatarContainer = styled.div<{ backgroundImage: string; backgroundColor: string; size: TStringOrNumber; }>`
+	position: relative;
+  border-radius: 9999px;
+  background-image: url('${props => props.backgroundImage}');
+	background-size: 100% 100%;
+  background-color: ${props => props.backgroundColor};
+  user-select: none;
+  width: ${({size}) => typeof size === 'string' ? size : `${size}px`};
+  height: ${({size}) => typeof size === 'string' ? size : `${size}px`};
 `;
 
 export const Abbreviation = styled(Body)`
-    width: 100%;
-    height: 100%;
-    display: flex;
-    justify-content: center;
-    align-items: center;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
