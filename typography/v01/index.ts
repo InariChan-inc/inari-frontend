07c2543f9e77749e4f242ffa1b73147502e6af4a
Vitalii Shulhan <vitalii.shulhan@gmail.com>
2021-10-07 17:43:28 +0200

    Added Helper and Caption typographies to export

--- typography/v01/index.ts
+++ typography/v01/index.ts
@@ -3,3 +3,5 @@ export { default as Button } from './Button';
 export { default as Headline } from './Headline';
 export { default as Link } from './Link';
 export { default as Subtitle } from './Subtitle';
+export { default as Helper } from './Helper';
+export { default as Caption } from './Caption';
