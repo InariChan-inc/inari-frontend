b34ad0154193146394fcf8bb0bdd5343c993fbfd
Vitalii Shulhan <vitalii.shulhan@gmail.com>
2021-10-07 23:43:13 +0200

    Added image for future feature screen

--- atoms/index.ts
+++ atoms/index.ts
@@ -2,3 +2,4 @@ export { default as Logo } from './Logo';
 export { default as Link } from './Link';
 export { default as Avatar } from './Avatar';
 export { default as Helmet } from './Helmet';
+export { default as FutureImage } from './FutureImage';
