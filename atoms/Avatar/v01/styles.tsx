c83f220628cc52a5e16eb546ab54883876f9c697
Vitalii Shulhan <vitalii.shulhan@gmail.com>
2021-10-29 01:04:39 +0200

    Set min dimensions for Avatar

--- atoms/Avatar/v01/styles.tsx
+++ atoms/Avatar/v01/styles.tsx
@@ -3,14 +3,17 @@ import { Body } from '@typography';
 import { TStringOrNumber } from "@common/types";
 
 export const AvatarContainer = styled.div<{ backgroundImage: string; backgroundColor: string; size: TStringOrNumber; }>`
+  display: flex;
+  justify-content: center;
+  align-items: center;
 	position: relative;
   border-radius: 9999px;
   background-image: url('${props => props.backgroundImage}');
 	background-size: 100% 100%;
   background-color: ${props => props.backgroundColor};
   user-select: none;
-  width: ${({size}) => typeof size === 'string' ? size : `${size}px`};
-  height: ${({size}) => typeof size === 'string' ? size : `${size}px`};
+  min-width: ${({size}) => typeof size === 'string' ? size : `${size}px`};
+  min-height: ${({size}) => typeof size === 'string' ? size : `${size}px`};
 `;
 
 export const Abbreviation = styled(Body)`
