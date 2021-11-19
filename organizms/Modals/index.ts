90c12ef88bb218506e9b626afab083a969a41a9c
Vitalii Shulhan <vitalii.shulhan@gmail.com>
2021-11-19 13:03:19 +0100

    Updated structure of modals: created Modals folder in organizms

---
+++ organizms/Modals/index.ts
@@ -0,0 +1,1 @@
+export { default as CropAvatarModal } from './CropAvatarModal';
--- organizms/index.ts
+++ organizms/index.ts
@@ -1,10 +1,12 @@
 import Authorization from './Authorization';
 import AccountControl from './AccountControl';
-import CropAvatarModal from './CropAvatarModal';
-
 
 export {
-    Authorization,
-    AccountControl,
-    CropAvatarModal,
+  Authorization,
+  AccountControl,
 };
+
+export {
+  CropAvatarModal
+} from './Modals';
+
