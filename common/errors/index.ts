938b025bfe69c0072c1ca3d2809e5b0ad33409fb
Vitalii Shulhan <vitalii.shulhan@gmail.com>
2021-12-14 19:26:31 +0100

    Fixed: added error constant

--- common/errors/index.ts
+++ common/errors/index.ts
@@ -1,3 +1,4 @@
 export const NOT_MATCHED_ERROR = 'NOT MATCHED';
 export const EXISTS_ERROR = 'ALREADY EXISTS';
 export const EMPTY_ERROR = 'CANNOT BE EMPTY';
+export const CANNOT_BE_EQUAL_ERROR = 'CANNOT BE EQUAL';
