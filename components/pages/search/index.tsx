005705ab89f2aa2d7f2c9f3dd729c60f52cea3e8
Vitalii Shulhan <vitalii.shulhan@gmail.com>
2022-01-07 18:23:31 +0100

    Search page: added ContentWrappe, GridWrapper and FiltersWrapper, extended SearchContainer

--- components/pages/search/index.tsx
+++ components/pages/search/index.tsx
@@ -1,9 +1,13 @@
 import styled from 'styled-components';
-import FilterListIcon from '@mui/icons-material/FilterList';
+import values from '@common/values';
 
 
 export const SearchContainer = styled.div`
+  display: flex;
+  flex-direction: column;
   padding: 32px 60px;
+  height: 100%;
+  position: relative;
 `;
 
 export const FilterToggleButton = styled.div`
@@ -17,8 +21,34 @@ export const UpControllerWrapper = styled.div`
   justify-content: space-between;
 `;
 
-export const FilterWrapper = styled.div`
+export const ContentWrapper = styled.div`
+  display: flex;
+  flex-grow: 1;
+  margin: 30px 0;
+`;
 
+export const GridWrapper = styled.div<{ isFilterOpen: boolean }>`
+  flex: 1;
+  ${({ isFilterOpen }) => isFilterOpen && 'margin-right: 82px;'}
+`;
+
+export const FiltersWrapper = styled.div<{ open: boolean }>`
+  display: ${({ open }) => open ? 'block': 'none'};
+  width: 305px;
+  padding: 35px;
+  height: fit-content;
+  max-height: calc(100vh - ${values.menuWidth}px - 1px - 32px - 36px - 30px);
+  border: 1px solid ${(props) => props.theme.colors['yellow-1']};
+  border-radius: 5px;
+  overflow: auto;
+  box-shadow: 0 16px 24px 0 ${(props) => props.theme.colors['yellow-1']};
+  position: sticky;
+  top: 30px;
+  overflow: auto;
+
+  > div:not(:last-of-type) {
+    margin-bottom: 10px;
+  }
 `;
 
 
