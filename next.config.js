518ffa2d9c3b6fc14b652bb62f9d85ea081d42b8
Vitalii Shulhan <vitalii.shulhan@gmail.com>
2022-01-07 23:49:59 +0100

    Removed connectings mui with styled-components

--- next.config.js
+++ next.config.js
@@ -2,11 +2,4 @@ module.exports = {
     env: {
         ...require('./env'),
     },
-    webpack: (config) => {
-        config.resolve.alias = {
-            ...config.resolve.alias,
-            '@mui/styled-engine': '@mui/styled-engine-sc',
-        };
-        return config;
-    }
 }
