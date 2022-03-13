867db642e3c8be6f8549fb2080032f3d66262e16
Vitalii Shulhan <vitalii.shulhan@gmail.com>
2022-03-13 15:24:00 +0100

    Implemented template for AnimeCardSection

---
+++ organisms/AnimeCardSection/v01/index.tsx
@@ -0,0 +1,19 @@
+import { VoidFunctionComponent } from "react";
+import {
+  AnimeCardSectionContainer,
+} from './styles';
+
+export interface AnimeCardSectionProps {
+
+}
+
+const AnimeCardSection: VoidFunctionComponent<AnimeCardSectionProps> = ({
+
+}) => (
+  <AnimeCardSectionContainer>
+    
+  </AnimeCardSectionContainer>
+);
+
+
+export default AnimeCardSection;
