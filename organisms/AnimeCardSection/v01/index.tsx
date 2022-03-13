451242836a80b03d150f288594142afc192e3198
Vitalii Shulhan <vitalii.shulhan@gmail.com>
2022-03-13 17:22:20 +0100

    Implemented AnimeCardSection

--- organisms/AnimeCardSection/v01/index.tsx
+++ organisms/AnimeCardSection/v01/index.tsx
@@ -1,17 +1,23 @@
 import { VoidFunctionComponent } from "react";
+import { Headline } from '@typography';
 import {
   AnimeCardSectionContainer,
+  AnimeCardsWrapper
 } from './styles';
+import { AnimeCard, AnimeCardProps } from "@molecules";
 
 export interface AnimeCardSectionProps {
-
+  animes: AnimeCardProps[];
 }
 
 const AnimeCardSection: VoidFunctionComponent<AnimeCardSectionProps> = ({
-
+  animes,
 }) => (
   <AnimeCardSectionContainer>
-    
+    <Headline type={2}>Рекомендуємо подивитися</Headline>
+    <AnimeCardsWrapper>
+      {animes.slice(0, 5).map(anime => <AnimeCard key={anime.id} {...anime} />)}
+    </AnimeCardsWrapper>
   </AnimeCardSectionContainer>
 );
 
